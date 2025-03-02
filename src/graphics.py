031c2460ef0418a4a4a8f04b298b76a12a60fa20
MedovMikhail <medovmikhail03@mail.ru>
2025-03-02 16:23:17 +0400

    python for grafik

---
+++ src/graphics.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+def get_lines_from_file(filename):
+    file = open(filename, 'r')
+    open_mp = [int(x) for x in file.readline().split('ns') if x.isdigit()]
+    linux = [int(x) for x in file.readline().split('ns') if x.isdigit()]
+    return [open_mp, linux]
+
+def creat_graphic(open_mp, linux):
+    x = list(range(5,5+len(open_mp)))
+
+    plt.xlabel("Размер матриц")
+    plt.ylabel("Время на перемножение в нс")
+
+    plt.plot(x, open_mp, label="OpenMP")
+    plt.plot(x, linux, label="Linux API")
+
+    plt.savefig("resources/pp_grahic.png")
+
+
+def main():
+    filename = "src/data.txt"
+    open_mp, linux = get_lines_from_file(filename)
+    if (len(open_mp) != len(linux)):
+        print("Different sizes of data!")
+        return
+    creat_graphic(open_mp, linux)
+    
+    
+
+if __name__ == "__main__":
+    main()
