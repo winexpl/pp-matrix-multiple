35fb748576b4513ffd06c08cf4cc2e9c5222f690
MedovMikhail <medovmikhail03@mail.ru>
2025-03-11 21:43:07 +0400

    last python commit

--- src/graphics.py
+++ src/graphics.py
@@ -2,15 +2,15 @@ import matplotlib.pyplot as plt
 
 def get_lines_from_file(filename):
     file = open(filename, 'r')
-    open_mp = [int(x) for x in file.readline().split('ns') if x.isdigit()]
-    linux = [int(x) for x in file.readline().split('ns') if x.isdigit()]
+    open_mp = [float(int(x)/1000000) for x in file.readline().split('ns') if x.isdigit()]
+    linux = [float(int(x)/1000000) for x in file.readline().split('ns') if x.isdigit()]
     return [open_mp, linux]
 
 def creat_graphic(open_mp, linux):
     x = list(range(5,5+len(open_mp)))
 
     plt.xlabel("Размер матриц")
-    plt.ylabel("Время на перемножение в нс")
+    plt.ylabel("Время на перемножение в мс")
 
     plt.plot(x, open_mp, label='OpenMP')
     plt.plot(x, linux, label='Linux API')
